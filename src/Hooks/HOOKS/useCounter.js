fafd99d6524c0b9599eaeed290dfa0aa41f8c63e
DeerK <derek.rodriguez.mendoza@est.una.ac.cr>
2025-03-14 17:32:53 -0600

    Segundo-Commit

---
+++ src/Hooks/HOOKS/useCounter.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react'
+
+export function useCounter() {
+  const [count, setCount] = useState(() => {
+    try {
+      return Number(window.localStorage.getItem('count')) || 0
+    } catch (error) {
+      return 0
+    }
+  })
+
+  useEffect(() => {
+    window.localStorage.setItem('count', count)
+  }, [count])
+
+  const increaseCount = () => setCount(prev => prev + 1)
+  const decreaseCount = () => setCount(prev => (prev > 0 ? prev - 1 : 0))
+  const resetCount = () => setCount(0) 
+  return { 
+    count, 
+    increaseCount, 
+    decreaseCount,
+    resetCount 
+  }
+}
